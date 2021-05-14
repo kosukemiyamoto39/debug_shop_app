7fd6544660abc8192822948a513c950321d714c5
kmiyamoto-eng <muryo40@gmail.com>
2021-05-15 08:33:06 +0900

    each文の変数を複数形に修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @item = Item.order(created_at: :desc)
+    @items = Item.order(created_at: :desc)
 
   end
 
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+
     Item.create!(item_params)
 
     redirect_to items_path
